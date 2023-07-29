62338c5201542656a417747d90ef39722aa329fc
Aryan Gupta <aryangupta07075@gmail.com>
2023-07-30 04:41:41 +0530

    added main and buyer css

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('about_us/', views.about_us, name='about_us'),
     path('get_started/', views.get_started, name='get_started'),
     path('buyer_or_seller/', views.buyer_or_seller, name='buyer_or_seller')
-]
+] 
