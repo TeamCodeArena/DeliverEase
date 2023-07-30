09c8f1af62fcf9623650af5bcbc6fe85c0c18cc3
Aryan Gupta <aryangupta07075@gmail.com>
2023-07-30 11:58:24 +0530

    fixed stylesheets linking

--- buyer/admin.py
+++ buyer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Buyer, Job, Seller
 # Register your models here.
+
+admin.site.register(Buyer)
+admin.site.register(Job)
+admin.site.register(Seller)
