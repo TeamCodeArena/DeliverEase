4b10783b430f9c4258e402e877a55ef7933a60ae
Aryan Gupta <aryangupta07075@gmail.com>
2023-08-09 13:08:17 +0530

    linted python code

--- userAuth/models.py
+++ userAuth/models.py
@@ -3,26 +3,28 @@ from django.db import models
 # Create your models here.
 
 
-## this is the buyer table contains the different details about the buyer
+# this is the buyer table contains the different details about the buyer
 class Buyer(models.Model):
     name = models.CharField(max_length=64)
     email = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
-    phoneNo = models.CharField(max_length=64)
+    phone_no = models.CharField(max_length=64)
     address = models.CharField(max_length=64)
 
     def __repr__(self) -> str:
-        return f"{self.name}  {self.id} {self.email} {self.phoneNo} {self.email} {self.address}"
+        return f"{self.name}  {self.id} {self.email} {self.phone_no} " \
+               f"{self.email} {self.address}"
 
 
-## this is the seller table contains the different details about the seller
+# this is the seller table contains the different details about the seller
 class Seller(models.Model):
     name = models.CharField(max_length=64)
     email = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
-    phoneNo = models.CharField(max_length=64)
+    phone_no = models.CharField(max_length=64)
     address = models.CharField(max_length=100)
     experience = models.CharField(max_length=100)
 
     def __repr__(self) -> str:
-        return f"{self.name}  {self.id} {self.email} {self.experience} {self.phoneNo}  {self.address}"
+        return f"{self.name}  {self.id} {self.email} {self.experience}" \
+               f" {self.phone_no}  {self.address}"
--- userAuth/urls.py
+++ userAuth/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-## these are the url patterns for the user authorization.
+# these are the url patterns for the user authorization.
 urlpatterns = [
     path("buyer_signup/", views.buyer_signup, name="buyer_signup"),
     path("seller_signup/", views.seller_signup, name="seller_signup"),
