6f25d7258da20f69dad002850cab868494927020
Aryan Gupta <aryangupta07075@gmail.com>
2023-08-14 14:40:01 +0530

    updated function names

--- main/tests.py
+++ main/tests.py
@@ -2,17 +2,17 @@ from django.test import TestCase, Client
 from django.db.models import Max
 
 class MainTestCase(TestCase):
-    def test_index(self):
+    def test_index_page(self):
         c = Client()
         response = c.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_about_us(self):
+    def test_about_us_page(self):
         c = Client()
         response = c.get("/about_us/")
         self.assertEqual(response.status_code, 200)
 
-    def test_get_started(self):
+    def test_get_started_page(self):
         c = Client()
         response = c.get('/get_started/')
         self.assertEqual(response.status_code, 200)
