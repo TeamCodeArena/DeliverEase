c1574939dfc14633b3890da4ad6b8de7a8849f3c
Aryan Gupta <aryangupta07075@gmail.com>
2023-08-14 14:39:01 +0530

    added test suite for the app: main GH-67

--- main/tests.py
+++ main/tests.py
@@ -1,2 +1,19 @@
+from django.test import TestCase, Client
+from django.db.models import Max
 
-# Create your tests here.
+class MainTestCase(TestCase):
+    def test_index(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us(self):
+        c = Client()
+        response = c.get("/about_us/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_started(self):
+        c = Client()
+        response = c.get('/get_started/')
+        self.assertEqual(response.status_code, 200)
+
